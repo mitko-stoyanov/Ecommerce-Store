9fd1c4831379352b402f527fc467592213624e0c
Mitko Stoyanov <mitko_st6@abv.bg>
2022-12-31 09:48:53 +0200

    Added requirements and helpers file.

---
+++ online_store/online_store/helpers.py
@@ -0,0 +1,10 @@
+class BootstrapFormMixin:
+    fields = {}
+
+    def _init_bootstrap_form_controls(self):
+        for _, field in self.fields.items():
+            if not hasattr(field.widget, 'attrs'):
+                setattr(field.widget, 'attrs', {})
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = ''
+            field.widget.attrs['class'] += ' form-control'
